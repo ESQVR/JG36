ae19ee9d823915d8477a072dc8e43b152aa0a8dc
Zachary Kubli <Kubli.Zachary@gmail.com>
2024-02-01 13:59:59 -0500

    fixed keys

--- src/sprites/character.js
+++ src/sprites/character.js
@@ -21,27 +21,7 @@ export class Character extends Phaser.Physics.Arcade.Sprite {
         this.setInteractive();
         this.setCollideWorldBounds(true);
 
-        export class Character extends Phaser.Physics.Arcade.Sprite {
-    static getTextures() {
-        let my_needs ={
-            "character": "assets/char.png",
-        }
-        return {
-            ...my_needs,
-            ...Candle.getTextures()
-        }
-    }
-
-    constructor(scene, x, y) {
-        super(scene, x, y, 'character');
-
-        scene.add.existing(this);
-        scene.physics.add.existing(this);
-
-        this.setInteractive();
-        this.setCollideWorldBounds(true);
-
-        this.cursors = this.scene.input.keyboard.addKeys('W,A,S,D');
+        this.cursors = this.scene.input.keyboard.addKeys('W,A,S,D,UP,DOWN,LEFT,RIGHT');
 
         // Create the candle
         this.candle = new Candle(scene, 400, 300);
@@ -56,37 +36,10 @@ export class Character extends Phaser.Physics.Arcade.Sprite {
         } else {
             this.setVelocityX(0);
         }
-
+    
         if (this.cursors.W.isDown || this.cursors.UP.isDown) {
             this.setVelocityY(-200);
         } else if (this.cursors.S.isDown || this.cursors.DOWN.isDown) {
-            this.setVelocityY(200);
-        } else {
-            this.setVelocityY(0);
-        }
-        
-        // Update the candle
-        this.candle.update();
-    }
-}
-
-        // Create the candle
-        this.candle = new Candle(scene, 400, 300);
-    }
-
-    update() {
-        // Character movement
-        if (this.cursors.A.isDown) {
-            this.setVelocityX(-200);
-        } else if (this.cursors.D.isDown) {
-            this.setVelocityX(200);
-        } else {
-            this.setVelocityX(0);
-        }
-
-        if (this.cursors.W.isDown) {
-            this.setVelocityY(-200);
-        } else if (this.cursors.S.isDown) {
             this.setVelocityY(200);
         } else {
             this.setVelocityY(0);
